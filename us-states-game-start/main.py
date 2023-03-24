544931780a7edb502dca023f7c126ad435084645
DamTran <thdam3012@gmail.com>
2023-03-24 23:27:56 +0200

    forth commit

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -1,6 +1,5 @@
 import turtle
 import pandas
-from clock import Clock
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -18,22 +17,28 @@ screen.tracer(0)
 # Import data
 data = pandas.read_csv("50_states.csv")
 # check match answer and write state
-point = 0
-while point < 50:
+guessed_states = []
+while len(guessed_states) < 50:
     screen.update()
-    answer = screen.textinput(title=f"{point}/50 States Correct", prompt="What's another state's name?")
+    answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?")
     answer = answer.title()
     if answer in data["state"].unique():
         index = data[data["state"] == answer].index
         print(index)
         pen.goto(int(data["x"][index]), int(data["y"][index]))
         pen.write(answer)
-        point += 1
-    elif answer == "Quit":
+        guessed_states.append(answer)
+    elif answer == "Exit":
+        # states_to_learn.csv
+        states_to_learn = []
+        for state in data["state"]:
+            if state not in guessed_states:
+                states_to_learn.append(state)
+        df = pandas.DataFrame(states_to_learn)
+        df.to_csv("States_to_learn.csv")
         break
     else:
         print("This is not a state")
 
 
 
-screen.exitonclick()
